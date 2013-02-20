88d6cdef94e0f3da65d0722ac9a2b5d74ab7c29a
LeonFedotov <fedotov.leon@gmail.com>
2013-02-20 06:02:41 +0200

    Wohooo progress!

--- js/calculateEventLayout.js
+++ js/calculateEventLayout.js
@@ -10,7 +10,7 @@ var layOutDay = (function(maxWidth, maxHeight) {
 					}
 
 					//between boundaries
-					return current.start >= 0 && current.end >= current.start;
+					return current.start >= 0 && current.end >= current.start && current.end <= maxHeight;
 
 				}).sort(function(a, b) {
 					//sorting by start time then by length.
@@ -21,29 +21,161 @@ var layOutDay = (function(maxWidth, maxHeight) {
 					}
 				});
 			},
-
-			collides = function(current, other) {
-				return other.start < current.start && other.end > current.start;
+			tree = [{start: 0, end: 720, siblings: []}],
+			collides = function(node, item) {
+				return (node.start < item.start && node.end > item.start) || 
+						(item.start == node.start && item.end == node.end) ||
+						(item.start == node.start) || (item.end == node.end);
 			},
+			
+			getSiblings = function(depth) {
+				for(var i in tree) {
+					if(tree[i].depth == depth) {
+						return tree[i].siblings.concat(tree[i]);
+					}
+				}
+				return [];
+			},
+
+			sibilingsCheck = function(item, depth) {
+				var siblings = getSiblings(depth);
+
+				for(var i in siblings) {
+					if(collides(siblings[i], item) || collides(item, siblings[i])) {
+						placeInTree(item, siblings[i], depth, true);
+						return false;
+					}
+				}
+				return true;
+			},
+
+			putUnder = function(node, item, depth) {
+
+				item.depth = depth+1;
+				if(sibilingsCheck(item, depth+1)) {
+					if(node.children) {
+						node.children.push(item);
+					} else {
+						node.children = [item];
+					}
+					item.parent = node;
+					updateSiblings(item, depth+1);
+					tree.push(item);
+				}
+			},
+			
+			putBesides = function(node, item, depth) {
+				if(item.id == 5) {debugger;}
+				console.log(item.id);
+				if(node.parent) {
+					node.parent.children.push(item);
+				}
+				item.depth = depth;
+				updateSiblings(item, depth);
+				tree.push(item);
+			},
+			updateSiblings = function(item, depth) {
+				item.siblings = tree.reduce(function(list, current) {
+					if(current.depth == depth) {
+						current.siblings.push(item)
+						list.push(current);
+					}
+					return list;
+				}, []);
+			},
+
+			placeInTree = function(item, node, depth, siblingSearch) {
+				if(collides(node, item)) {
+					if(node.children) {
+						for(var i in node.children) {
+							if(placeInTree(item, node.children[i], depth+1, siblingSearch)) {
+								return true;
+							}
+						}
+					} else if(node.siblings.length && !siblingSearch) {
+						for(var j in node.siblings) {
+							if(placeInTree(item, node.siblings[j], depth, true)) {
+								return true;
+							}
+						}
+					} 
+
+					putUnder(node, item, depth);
+					return true;
+				} else {
+					if(node.siblings.length && !siblingSearch) {
+						for(var j in node.siblings) {
+							if(placeInTree(item, node.siblings[j], depth, true)) {
+								return true;
+							}
+						}
+						putBesides(node, item, depth);
+						return true;
+					} 
+
+					if(!siblingSearch) {
+						putBesides(node, item, depth);
+						return true;
+					} else {
+						return false;
+					}
+				}
+				console.log('w00t?');
+				debugger;
+			},
+
 			scanAndPlace = function(data) {
-				
-				return data;
+				data.forEach(function(item) {
+					
+					placeInTree(item, tree[0], -1);
+				});
+
+				console.log(tree);
 			},
 
-			format = function(data) {
-				return data.map(function(current, index) {
-					var left = ;
+			maxSiblings = function() {
+				tree.forEach(function(current) {
+					var max = 0, count;
+					for(var seek = current.start; seek < current.end;seek++) {
+						count = tree.reduce(function(count, other) {
+							if(current.id != other.id && other.start < seek && other.end > seek) {
+								count++;
+							}
+							return count;
+						}, 0);
+						max = max > count ? max : count;
+
+					}
+					current.maxSiblings = max;
+				});
+
+				tree.forEach(function(current) {
+					for(var seek = current.start; seek < current.end; seek++) {
+						current.maxSiblings = tree.reduce(function(count, other) {
+							if(current.id != other.id && other.start < seek && seek < other.end) {
+								count = count > other.maxSiblings?count:other.maxSiblings;
+							}
+							return count;
+						}, current.maxSiblings);
+					}
+				});
+			},
+			format = function() {
+				tree.shift();
+				maxSiblings();
+				return tree.map(function(current, index) {
+					var width = maxWidth / (current.maxSiblings+1);
 					return {
 						id     : current.id,
 						top    : current.start,
-						width  : current.width,
-						left   : current.left,
+						width  : width,
+						left   : current.depth*width,
 						start  : current.start,
 						end    : current.end,
 						height : current.end - current.start,
 						//debugging
-						pos: current.pos,
-						siblings: current.columns,
+						pos: current.depth,
+						siblings: current.maxSiblings+1,
 						index: index
 					};
 				});
@@ -51,8 +183,8 @@ var layOutDay = (function(maxWidth, maxHeight) {
 
 		return function(data) {
 			var sorted = filterAndSort(data);
-			var result = scanAndPlace(sorted);
-			return format(result);
+			scanAndPlace(sorted);
+			return format();
 		};
 	}(600, 720)),//maxWidth, maxHeight
 	addEvents  = function(target, events) {
@@ -63,7 +195,7 @@ var layOutDay = (function(maxWidth, maxHeight) {
 				width  : event.width  + 'px',
 				height : event.height + 'px'
 			});
-			$('<dl>').append('<dt>'+event.id+':i:'+event.index+'Sample item</dt><dd>Sample location start:'+event.start+', end:'+event.end+', pos:'+event.pos+', columns:'+event.siblings+'</dd>').appendTo(wrapper);
+			$('<dl>').append('<dt>'+event.id+':i:'+event.index+'Sample item</dt><dd>Sample location start:'+event.start+', end:'+event.end+', pos:'+event.pos+', siblings:'+event.siblings+'</dd>').appendTo(wrapper);
 
 			wrapper.appendTo(target);
 		});
